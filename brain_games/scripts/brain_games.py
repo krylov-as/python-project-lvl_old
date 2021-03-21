93b61a4a833f5fc8f7fa8151e3018db6004b7115
mmf.krylovas <mmf.krylovas@gmail.com>
2021-03-21 13:36:52 +0300

    second version brain-games

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,11 +1,16 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
+import sys
+sys.path.append('/home/krylovas/python-project-lvl1/brain_games')
+
+import cli
 
 def welcome():
     print('Welcome to the Brain Games!')
-
+    
 
 def main():
     welcome()
+    cli.welcome_user()
 
 
 if __name__ == '__main__':
